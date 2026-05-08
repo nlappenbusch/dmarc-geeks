63f862af81b309dc57d63037708383581798ec0e
Nils Lappenbusch <nlappenbusch@gmail.com>
2026-05-08 08:16:55 +0200

    deploy.sh: build + down + up statt up --build (idempotent gegen Port-Bind-Race)

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,12 @@ cd "$REPO_DIR"
 git fetch origin main
 git reset --hard origin/main
 
-# Build + (Re)Start Container
+# Image bauen, danach sauber down + up. Trennen verhindert Port-Bind-Konflikte
+# wenn der alte Container die Ports noch nicht freigegeben hat.
 docker compose pull || true
-docker compose up -d --build
+docker compose build
+docker compose down --remove-orphans
+docker compose up -d
 
 # Alte Images aufraeumen
 docker image prune -f
